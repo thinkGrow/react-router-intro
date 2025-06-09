f86e5068aa3970b2dc6bb7851da5dcec22c499a9
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-10 05:48:54 +0600

    outlet routing

---
+++ src/components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mobiles = () => {
+  return (
+    <div>
+      <h2>Mobile</h2>
+    </div>
+  );
+};
+
+export default Mobiles;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,36 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Mobiles from "./components/Mobiles/Mobiles.jsx";
+import Home from "./components/Home/Home.jsx";
+import Root from "./components/Root/Root.jsx";
+import Laptops from "./components/Laptops/Laptops.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello from react router</div>,
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "laptops", Component: Laptops},
+    ],
+  },
+  {
+    path: "about",
+    element: <div>About me</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs are here</div>,
+  },
+  {
+    path: "app",
+    element: <App></App>,
+  },
+  {
+    path: "app/:id",
+    Component: App,
   },
 ]);
 
